14ac84c9e643efa2f12f79ed258101cbe0f625be
lsk222 <3295113815@qq.com>
2020-06-01 22:17:00 +0800

    轮播图开始

---
+++ src/js/lunbo.js
@@ -0,0 +1,12 @@
+class lunbo{
+    constructor(){
+        this.lunboUl=$('.lunbo');
+        this.lunboLis=$('.lunbo li');
+        this.btns=$('.btn li');
+    }
+    init(){
+        let pic=$('')
+    }
+   
+    
+}
