3889ce3e0e274338d9766ff4f0b07c0540489a7f
lsk222 <3295113815@qq.com>
2020-06-04 08:12:27 +0800

    lunbotu

--- src/js/lunbo.js
+++ src/js/lunbo.js
@@ -1,12 +1,62 @@
 class lunbo{
     constructor(){
-        this.lunboUl=$('.lunbo');
+        this.imgBox=$('.img-box');
+        this.lunboUl=$('#main-body .lunbo');
         this.lunboLis=$('.lunbo li');
         this.btns=$('.btn li');
+        this.index=0;
     }
     init(){
-        let pic=$('')
+        let copyLi=this.lunboLis.first().clone(true,true);
+        let mytimer=-1;
+        console.log(this.lunboUl);
+         this.lunboUl.append(copyLi).css({
+            width:this.lunboLis.first().width()*this.lunboUl.children().length+'px'
+        });
+       
+        let _this=this;
+        //按钮悬浮 切换图片
+        this.btns.on('mouseover',function(){
+            _this.index=$(this).index();
+            console.log($(this).index());
+            lunBoSwitch();
+        });
+        autoPlay();
+        function  lunBoSwitch(){
+            if(_this.index>_this.lunboLis.length){
+                _this.index=0;
+                _this.lunboUl.css({
+                    left:0
+                })
+                _this.index=1;
+
+            }
+            _this.lunboUl.animate({
+                left:-_this.lunboLis.first().width()*_this.index+'px'
+            },600);
+            _this.btns.eq(_this.index%_this.lunboLis.length).css({
+                backgroundColor:'red'
+            }).siblings().css({
+                backgroundColor:'grey'
+            });
+        }
+
+        
+        function autoPlay(){
+            mytimer=setInterval(function(){
+                console.log(111);
+                _this.index++;
+                lunBoSwitch();
+            },3000);
+        }
+        this.imgBox.hover(function(){
+            console.log('stop');
+            clearInterval(mytimer);
+        },function(){
+            autoPlay();
+        });
+       
     }
-   
-    
+      
 }
+new lunbo().init();
